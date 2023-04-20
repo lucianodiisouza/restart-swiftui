94080a95e60aab8aee61ee0f7ab803752068b700
Luciano dii Souza <lucianodiisouza@hotmail.com>
2023-04-20 04:42:26 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Luciano dii Souza on 20/04/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
