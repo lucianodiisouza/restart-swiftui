448d4649569de59f5664c52de806a5825b057077
Luciano dii Souza <lucianodiisouza@hotmail.com>
2023-04-21 16:06:12 -0300

    wip: animating itens

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -1,29 +1,28 @@
-//
 //  OnboardingView.swift
 //  Restart
-//
 //  Created by Luciano dii Souza on 20/04/23.
-//
 
 import SwiftUI
 
 struct OnboardingView: View {
-    
+    // MARK: - PROPERTY
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
     
     @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
     @State private var buttonOffset: CGFloat = 0
+    @State private var isAnimating: Bool = false
     
     var body: some View {
         ZStack {
             Color("ColorBlue")
                 .ignoresSafeArea(.all, edges: .all)
             
+            // MARK: - HEADER
             VStack(spacing: 20) {
                 Spacer()
                 
                 VStack(spacing: 0) {
-                Text("Share")
+                    Text("Share")
                         .font(.system(size: 60))
                         .fontWeight(.heavy)
                         .foregroundColor(.white)
@@ -37,18 +36,26 @@ struct OnboardingView: View {
                     .foregroundColor(.white)
                     .multilineTextAlignment(.center)
                     .padding(.horizontal, 10)
-                }
+                } //: HEADER
+                .opacity(isAnimating ? 1 : 0)
+                .offset(y: isAnimating ? 0 : -40)
+                .animation(.easeOut(duration: 1), value: isAnimating)
                 
+                
+                // MARK: - CENTER
                 ZStack {
                     CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
                     
                     Image("character-1")
                         .resizable()
                         .scaledToFit()
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.easeOut(duration: 0.5), value: isAnimating)
                 }
                 
                 Spacer()
                 
+                // MARK: BUTTON
                 ZStack {
                     Capsule()
                         .fill(Color.white.opacity(0.2))
@@ -63,13 +70,13 @@ struct OnboardingView: View {
                         .offset(x: 20)
                     
                     HStack {
-                       Capsule()
+                        Capsule()
                             .fill(Color("ColorRed"))
                             .frame(width: buttonOffset + 80)
                         
                         Spacer()
                     }
-                    
+                    // MARK: SWIPEABLE BUTTON
                     HStack {
                         ZStack {
                             Circle()
@@ -81,7 +88,7 @@ struct OnboardingView: View {
                             Image(systemName: "chevron.right.2")
                                 .font(.system(size: 24, weight: .bold))
                         }
-                            .foregroundColor(.white)
+                        .foregroundColor(.white)
                         .frame(width: 80, height: 80, alignment: .center)
                         .offset(x: buttonOffset)
                         .gesture(
@@ -101,14 +108,21 @@ struct OnboardingView: View {
                                     }
                                 }
                         )
-                        
+                        // MARK: SPACER
                         Spacer()
                     }
                 }
                 .frame(width: buttonWidth, height: 80, alignment: .center)
                 .padding()
+                .opacity(isAnimating ? 1 : 0)
+                .offset(y: isAnimating ? 0 : 40)
+                .animation(.easeOut(duration: 1), value: isAnimating)
             }
         }
+        .onAppear(perform: {
+            isAnimating = true
+            }
+        )
     } 
 }
 
