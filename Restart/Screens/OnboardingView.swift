ecaa04f054db22dbf5cf71407d8ddf6303f3db67
Luciano dii Souza <lucianodiisouza@hotmail.com>
2023-04-21 17:36:55 -0300

    wip: rotating char1

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -53,6 +53,7 @@ struct OnboardingView: View {
                         .opacity(isAnimating ? 1 : 0)
                         .animation(.easeOut(duration: 0.5), value: isAnimating)
                         .offset(x: imageOffset.width * 1.2, y: 0)
+                        .rotationEffect(.degrees(Double(imageOffset.width / 20)))
                         .gesture(DragGesture()
                             .onChanged {
                                 gesture in
