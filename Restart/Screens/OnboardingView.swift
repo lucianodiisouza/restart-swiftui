938ddbeb12750cb902545ea370ab4d6589c07135
Luciano dii Souza <lucianodiisouza@hotmail.com>
2023-04-21 17:38:34 -0300

    feat: animating CircleGroupView

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -46,6 +46,9 @@ struct OnboardingView: View {
                 // MARK: - CENTER
                 ZStack {
                     CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+                        .offset(x: imageOffset.width * -1)
+                        .blur(radius: abs(imageOffset.width / 5))
+                        .animation(.easeOut(duration: 1), value: imageOffset)
                     
                     Image("character-1")
                         .resizable()
