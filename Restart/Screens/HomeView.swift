9f40f5a8ba5436733f0692a6321771cf32cfc336
Luciano dii Souza <lucianodiisouza@hotmail.com>
2023-04-20 22:35:12 -0300

    wip: design stuff for HomeView

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -11,14 +11,36 @@ struct HomeView: View {
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
     var body: some View {
         VStack(spacing: 20) {
-            Text("This is my HomeView!!!")
-                .font(.largeTitle)
+            Spacer()
+            
+            Image("character-2")
+                .resizable()
+                .scaledToFit()
+                .padding()
+            
+            Text("The time that leads to mastery is depedent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            Spacer()
             
             Button(action: {
                 isOnboardingViewActive = true
             }) {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                
                 Text("Restart")
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
             }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
+            
         }
     }
 }
